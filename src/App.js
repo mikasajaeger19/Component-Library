dc254ab6f759602800268b3c272a7c1a2e2d5abc
mikasajaeger19 <naeemniaz2012@gmail.com>
2024-03-14 16:33:11 +0530

    login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card';
+import Card from './components/Card/Card';
+import Login from './components/Login/Login';
 function App() {
   return (
     <div className="App">
-      <Card />
+      {/* <Card /> */}
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import './Login.css'
+
+const Login = () => {
+
+  const [userDetails, setUserDetails] = useState({
+    email : '',
+    password : '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post('login_api_call', userDetails);
+        console.log(response);
+        if(response.data.token)
+            localStorage.setItem('token', response.data.token);
+    }
+    catch (err) {
+        console.log(err);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserDetails({
+        ...userDetails,
+        [name] : value,
+    });
+  };
+  
+  return (
+    <div className='login-container'>
+      <div className='login-header'>
+        <h1>
+            LOGIN
+        </h1>
+      </div>
+      <div className='login-inputs'>
+            <input 
+                type='text' 
+                placeholder='EMAIL' 
+                className='login_input'
+                onChange={handleChange}
+                required
+            />  
+            <input 
+                type='text' 
+                placeholder='PASSWORD' 
+                className='login_input'
+                onChange={handleChange}
+                required
+            />
+        <button>
+            SUBMIT
+        </button>
+      </div>
+      <div className='login-footer'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
